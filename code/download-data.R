046404459fffc60a8427f73e5c76fb49bda92725
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2023-10-15 20:53:52 +0100

    Add initial code

---
+++ code/download-data.R
@@ -0,0 +1,3 @@
+# Download processed eqtlgen data from (Fauman and Hyde, 2022)
+download.file("https://static-content.springer.com/esm/art%3A10.1186%2Fs12859-022-04706-x/MediaObjects/12859_2022_4706_MOESM3_ESM.txt",
+              "./data/eqtlgen.tsv")
