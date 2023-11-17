55c7bffa66594aea2fd69d1204037bd60131212e
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2023-11-17 15:09:09 +0000

    Add ability to run snakemake locally

--- run.sh
+++ run.sh
@@ -4,4 +4,25 @@ source ~/.bashrc
 module load R/4.2.0
 micromamba activate snakemake
 
-snakemake -s Snakefile.py --profile none
+local_flag=''
+
+print_usage() {
+  printf "Usage: -l to run locally otherwise submits to server\n"
+}
+
+while getopts 'l' flag; do
+  case "${flag}" in
+    l) local_flag='true' ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+if [[ "$local_flag" = "true" ]]
+then
+  snakemake -s Snakefile.py --profile none --workflow-profile none --cores 1
+else
+  snakemake -s Snakefile.py --profile none
+fi
+
+
