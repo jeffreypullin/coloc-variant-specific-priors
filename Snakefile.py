762215fe8df8a4b1dc9a8d0d72de0aebefbc067b
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2023-11-23 15:57:37 +0000

    Update Snakefile
    with a variety of changes

--- Snakefile.py
+++ Snakefile.py
@@ -1,19 +1,16 @@
 configfile: "config.yaml"
 
 chromosomes = ["chr" + str(x) for x in range(1, 23)] + ["chrX"]
-endothelial_celltypes = ["Lymphatic", "Systemicvenous", "aCap", "arteriole", "gCap", "venule"]
 
 rule all: 
   input: 
     "data/plot-data/eqtl-catalogue.rds",
     "data/plot-data/gtex.rds",
     "data/plot-data/adipos-express-marginal.rds",
+    "data/plot-data/eqtlgen.rds",
+    "data/plot-data/onek1k.rds",
     "data/fauman-hyde/eqtlgen.txt",
-    "data/eqtlgen.txt",
-    "data/onek1k.tsv",
-    expand("data/adipos-express/processed-marginal-eur/{chromosome}.txt", chromosome = chromosomes),
-    expand("data/adipos-express/ab1-eur/{chromosome}.txt", chromosome = chromosomes),
-    expand("data/lung-single-cell/endothelial/{celltype}.txt", celltype = endothelial_celltypes)
+    expand("data/adipos-express/processed-ab1-eur/{chromosome}.txt", chromosome = chromosomes),
 
 rule download_fauman_hyde_eqtlgen_data: 
   output: "data/fauman-hyde/eqtlgen.txt"
@@ -30,6 +27,11 @@ rule download_eqtlgen_data:
     gunzip -d 2019-12-11-cis-eQTLsFDR0.05-ProbeLevel-CohortInfoRemoved-BonferroniAdded.txt.gz
     mv 2019-12-11-cis-eQTLsFDR0.05-ProbeLevel-CohortInfoRemoved-BonferroniAdded.txt {output}
     """
+    
+rule extract_plot_data_eqtlgen:
+  input: eqtlgen_path =  "data/eqtlgen.txt"
+  output: plot_data_path = "data/plot-data/eqtlgen.rds"
+  script: "code/extract-plot-data-eqtlgen.R"
     
 rule download_gtex_tar: 
   output: temporary("data/gtex-v8.tar")
@@ -59,10 +61,8 @@ rule download_etl_catalogue_metadata:
     """
 
 rule download_eqtl_catalogue_files: 
-  input: 
-    metadata_file = "data/eqtl-catalogue/eqtl-catalogue-metadata.tsv"
-  output: 
-    dataset_file = "data/eqtl-catalogue/sumstats/{dataset_id}.cc.tsv.gz"
+  input: metadata_file = "data/eqtl-catalogue/eqtl-catalogue-metadata.tsv"
+  output: dataset_file = "data/eqtl-catalogue/sumstats/{dataset_id}.cc.tsv.gz"
   script: "code/download-eqtl-catalogue.R"
   
 rule process_eqtl_catalogue_files:
@@ -83,13 +83,10 @@ rule extract_plot_data_eqtl_catalogue:
   script: "code/extract-plot-data-eqtl-catalogue.R"
   
 rule download_adipos_express_tars: 
-  output: 
-    marginal = temporary("data/adipos-express-marginal-eur-by-chr.tar"),
-    ab1 = temporary("data/adipos-express-ab1-eur-by-chr.tar")
+  output: temporary("data/adipos-express-marginal-eur-by-chr.tar"),
   shell:
     """
-    wget ftp://mohlkeanon:anon@rc-ns-ftp.its.unc.edu/marginal_byChr_EURonly.tar.gz -O - | gunzip -d > {output.marginal}
-    wget ftp://mohlkeanon:anon@rc-ns-ftp.its.unc.edu/AB1_byChr_EURonly.tar.gz -O - | gunzip -d > {output.ab1}
+    wget ftp://mohlkeanon:anon@rc-ns-ftp.its.unc.edu/marginal_byChr_EURonly.tar.gz -O - | gunzip -d > {output}
     """
 
 rule extract_adipos_express_marginal_files:
@@ -117,37 +114,14 @@ rule extract_plot_data_adipos_express_marginal:
     plot_data_path = "data/plot-data/adipos-express-marginal.rds"
   script: "code/extract-plot-data-adipos-express-marginal.R"
   
-rule extract_adipos_express_ab1_files:
-  input: "data/adipos-express-ab1-eur-by-chr.tar",
-  output: "data/adipos-express/ab1-eur/{chromosome}.txt"
-  shell:
-    """
-    tar -Oxf {input} AB1_byChr_EURonly/EURonly_AB1_local_eQTL_meta_{wildcards.chromosome}.txt > \
-    {output}
-    """
-
 rule download_onek1k_data:
   output: "data/onek1k.tsv"
   shell:
     """
     wget https://onek1k.s3.ap-southeast-2.amazonaws.com/esnp/esnp_table.tsv.gz -O - | gunzip -c > {output}
     """
-
-rule download_lung_endothelial_tar:
-  output: temporary("data/endothelial.tar")
-  shell:
-    """
-    wget "ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE227nnn/GSE227136/suppl/GSE227136%5Flimix%5Fres%5Fendothelial.tar.gz" -O - | gunzip -d > {output}
-    """
     
-rule extract_lung_endothelial_files:
-  input: "data/endothelial.tar"
-  output: "data/lung-single-cell/endothelial/{celltype}.txt"
-  shell:
-    """
-    tar -Oxf {input} endothelial_{wildcards.celltype}_qtl_results_all.txt > {output}
-    """
-
-
-    
-
+rule extract_plot_data_onek1k:
+  input: onek1k_path =  "data/onek1k.tsv"
+  output: plot_data_path = "data/plot-data/onek1k.rds"
+  script: "code/extract-plot-data-onek1k.R"
